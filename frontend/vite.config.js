b9635776416a613c39474160ec7d4c9d6a471219
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2024-11-24 16:24:00 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Default base path
   build: {
-    outDir: "dist",
+    outDir: "dist", // Ensure build output is in the dist directory
   },
 });
