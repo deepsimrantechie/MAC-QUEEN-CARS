48d4e35affb2fe54033e20efc7f23ced494584a6
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2024-11-24 22:45:33 +0530

    removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/", // Default base path
-  build: {
-    outDir: "dist", // Ensure build output is in the dist directory
-  },
 });
