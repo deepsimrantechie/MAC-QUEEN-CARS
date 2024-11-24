ca32f4f0d33947685d5c132d71239802ad92ef93
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2024-11-24 14:45:50 +0530

    added

--- backend/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("DB CONNECTED");
-  });
-  await mongoose.connect(`${process.env.MONGODB_URL}/CARS`);
-};
-export default connectDB;
